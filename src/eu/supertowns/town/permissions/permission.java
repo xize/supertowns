8feb605e97205c56c92f072d747fbd124c1250e4
xize <glucassen@live.nl>
2013-07-31 21:07:50 +0200

    fixed permission errors

--- src/eu/supertowns/town/permissions/permission.java
+++ src/eu/supertowns/town/permissions/permission.java
@@ -13,17 +13,13 @@ public class permission {
 			} else if(args.length == 1) {
 				if(args[0].equalsIgnoreCase("help")) {
 					sender.sendMessage(ChatColor.RED + "you are not allowed to use this command: /" + cmd.getName() +" "+ args[0] + "\n" + ChatColor.GRAY + "permission: supertowns.commands.help");
+				} else if(args[0].equalsIgnoreCase("claim")) {
+					sender.sendMessage(ChatColor.RED + "you are not allowed to use this command: /" + cmd.getName() +" "+ args[0] + "\n" + ChatColor.GRAY + "permission: supertowns.command.claim");
 				}
 			} else if(args.length == 2) {
 				if(args[0].equalsIgnoreCase("new")) {
 					sender.sendMessage(ChatColor.RED + "you are not allowed to use this command: /" + cmd.getName() +" "+ args[0] + "\n" + ChatColor.GRAY + "permission: supertowns.command.newtown");
-				}
-			} else if(args.length == 1) {
-				if(args[0].equalsIgnoreCase("claim")) {
-					sender.sendMessage(ChatColor.RED + "you are not allowed to use this command: /" + cmd.getName() +" "+ args[0] + "\n" + ChatColor.GRAY + "permission: supertowns.command.claim");
-				}
-			} else if(args.length == 2) {
-				if(args[0].equalsIgnoreCase("spawn")) {
+				} else if(args[0].equalsIgnoreCase("spawn")) {
 					sender.sendMessage(ChatColor.RED + "you are not allowed to use this command: /" + cmd.getName() +" "+ args[0] + "\n" + ChatColor.GRAY + "permission: supertowns.command.spawn");
 				}
 			}
